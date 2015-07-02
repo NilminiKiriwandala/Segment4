4f8fefd95627d10c5a24937ad4641665c8069fb7
Nilmini Kiriwandala <kiriwandalan@yahoo.com>
2015-07-02 11:54:18 +0530

    sd
    sdf

---
+++ Segment4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Segment4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.listView = new System.Windows.Forms.ListView();
+            this.txtChar = new System.Windows.Forms.TextBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Enter a char";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Location = new System.Drawing.Point(161, 38);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(124, 108);
+            this.lblDisplay.TabIndex = 9;
+            this.lblDisplay.Text = "Display Sorted list";
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(119, 134);
+            this.listView.TabIndex = 8;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            // 
+            // txtChar
+            // 
+            this.txtChar.Location = new System.Drawing.Point(232, 15);
+            this.txtChar.MaxLength = 1;
+            this.txtChar.Name = "txtChar";
+            this.txtChar.Size = new System.Drawing.Size(100, 20);
+            this.txtChar.TabIndex = 7;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(338, 15);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Sort List";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 155);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.txtChar);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.TextBox txtChar;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ Segment4/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Segment4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView.Items.Add("Apple");
+            listView.Items.Add("Banana");
+            listView.Items.Add("Orange");
+            listView.Items.Add("asparagus");
+            listView.Items.Add("beetroot");
+            listView.Items.Add("Black-eye been");
+            listView.Items.Add("Cherry");
+            listView.Items.Add("clementine");
+          
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (txtChar.Text != string.Empty)
+            {
+                char val = txtChar.Text.ToString()[0];
+                List<string> originalList = new List<string>();
+
+                foreach (ListViewItem item in listView.Items)
+                {
+                    originalList.Add(item.SubItems[0].Text.ToString());
+                }
+                List<string> sortedList = Sort(originalList, val);
+
+                string listItems = string.Empty;
+                foreach (string item in sortedList)
+                {
+                    listItems = listItems + "\n" + item;
+                }
+                lblDisplay.Text = listItems;
+            }
+        }
+        public List<string> Sort(List<string> theList, char theChar)
+        {
+            List<string> resultList = new List<string>();
+
+            foreach (string item in theList)
+            {
+                if (item[0] == (theChar.ToString().ToUpper())[0] || item[0] == (theChar.ToString().ToLower())[0])
+                {
+
+                    resultList.Add(item);
+
+                }
+            }
+            return resultList;
+        }
+    }
+}
